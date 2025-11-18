5d7c221fa2a6c7e7edda61e2f09894b0a8d84dbc
Giuseppe <cresctelli@gmail.com>
2025-11-18 20:17:08 +0100

    Built responsive design for about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,8 +11,8 @@ export default function page() {
       <Container>
         <Title>About us</Title>
 
-        <div className="grid grid-cols-2 gap-x-20 items-center mt-20">
-          <div className="w-full aspect-3/2 relative rounded-md overflow-hidden">
+        <div className="flex flex-col md:grid grid-cols-2 gap-x-20 items-center mt-20">
+          <div className="w-full aspect-3/2 max-md:max-w-[400px] relative rounded-md overflow-hidden">
             <Image
               src="/about.jpg"
               alt="Image of people working together"
@@ -20,17 +20,17 @@ export default function page() {
               fill
             />
           </div>
-          <div>
-            <h2 className="text-4xl font-bold">
+          <div className="max-md:text-center">
+            <h2 className="text-3xl max-md:mt-10 lg:text-4xl font-bold">
               We are a <span className="text-primary-500">strong</span> team
             </h2>
-            <p className="mt-10 text-lg">
+            <p className="mt-10 lg:text-lg">
               LuminaTech nasce dall&apos;idea di un team di ingegneri e designer
               con la passione per la tecnologia sostenibile. Crediamo che ogni
               ambiente meriti la giusta luce senza compromettere
               l&apos;ambiente.
             </p>
-            <p className="mt-10 text-lg">
+            <p className="mt-10 lg:text-lg">
               Per questo sviluppiamo soluzioni che uniscono efficienza,
               innovazione e design, rendendo l&apos;illuminazione smart
               accessibile a tutti. La nostra priorità è creare prodotti
@@ -57,9 +57,9 @@ export default function page() {
             innovation
           </span>
         </h3>
-        <div className="mt-20 flex gap-30">
-          <div className="flex flex-col gap-2 shadow-lg border border-gray-300 rounded-md">
-            <div className="relative w-[250px] aspect-4/5">
+        <div className="mt-20 grid grid-cols-1 min-[830px]:grid-cols-3 max-w-[1000px] w-full gap-10 justify-between">
+          <div className="flex flex-col max-w-[300px] mx-auto gap-2 shadow-lg border border-gray-300 rounded-md">
+            <div className="relative w-full aspect-square">
               <Image
                 src="/person-1.jpg"
                 alt="Person of our team"
@@ -68,15 +68,15 @@ export default function page() {
               />
             </div>
             <div className="p-4 grid grid-cols-2 items-center gap-y-2">
-              <h4 className="font-bold text-2xl col-span-2">Josh Armen</h4>
+              <h4 className="font-bold text-lg col-span-2">Josh Armen</h4>
               <span className="text-gray-500 text-lg">CEO</span>
               <span className="justify-self-end text-gray-500">
                 - Since 2019
               </span>
             </div>
           </div>
-          <div className="flex flex-col gap-2 shadow-lg border border-gray-300 rounded-md">
-            <div className="relative w-[250px] aspect-4/5">
+          <div className="flex flex-col max-w-[300px] mx-auto gap-2 shadow-lg border border-gray-300 rounded-md">
+            <div className="relative w-full aspect-square">
               <Image
                 src="/person-2.jpg"
                 alt="Person of our team"
@@ -85,24 +85,24 @@ export default function page() {
               />
             </div>
             <div className="p-4 grid grid-cols-2 items-center gap-y-2">
-              <h4 className="font-bold text-2xl col-span-2">Lucy Keller</h4>
+              <h4 className="font-bold text-lg col-span-2">Lucy Keller</h4>
               <span className="text-gray-500 text-lg">Architect</span>
               <span className="justify-self-end text-gray-500">
                 - Since 2021
               </span>
             </div>
           </div>
-          <div className="flex flex-col gap-2 shadow-lg border border-gray-300 rounded-md">
-            <div className="relative w-[250px] aspect-4/5">
+          <div className="flex flex-col max-w-[300px] mx-auto gap-2 shadow-lg border border-gray-300 rounded-md">
+            <div className="relative w-full aspect-square">
               <Image
                 src="/person-3.jpg"
                 alt="Person of our team"
-                className="object-cover object-center"
+                className="object-cover object-top"
                 fill
               />
             </div>
             <div className="p-4 grid grid-cols-2 items-center gap-y-2">
-              <h4 className="font-bold text-2xl col-span-2">Carl Monternat</h4>
+              <h4 className="font-bold text-lg col-span-2">Carl Monternat</h4>
               <span className="text-gray-500 text-lg">Manager</span>
               <span className="justify-self-end text-gray-500">
                 - Since 2020
